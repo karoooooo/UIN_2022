70860bd55baaa96bc2edf5ab2b7bf1102566e29c
Karoline <kanojo94@gmail.com>
2022-03-12 14:42:52 +0100

    Oppdatering og opprydding

---
+++ todo_k/src/App.js
@@ -0,0 +1,15 @@
+import MyComponent from './components/MyComponent'
+
+function App() {
+  // Write JavaScript, use Hooks, add state and more
+
+  return (
+    <main className="mx-auto max-w-sm mt-6 border-2 p-6">
+      <p className="text-3xl font-bold underline">Add JSX</p>
+      <p>Add components</p>
+      <MyComponent />
+    </main>
+  )
+}
+
+export default App
---
+++ u8_1_k/src/components/Actions.js
@@ -0,0 +1,42 @@
+   //skrive liste over innhold og id/key på artiklene
+    //når du klikker på button med samme key value så skal innohldet på den key'en med artikkel vises og mappes
+  
+
+ //pressButton fører til at de velger det nummeret, samme tall, det skjer en endring som er at visibility endres. 
+ //måte å sjekke om det er det samme tallet, putt inn den riktige objekt
+
+
+//State Visibility og standard vises ikke text. Text or hidden text. Selected shown text. viewArticle hvis text skal vises. 
+
+
+const Actions = ({event, useButton, hideText}) => {
+
+  event.target.key.value
+
+  <Display no={no} /> 
+  const viewArticle () => {
+
+  }  {useButton}
+
+  
+hideText
+
+  colorChange 
+  
+    
+  
+  
+
+    //er key value den samme på knapp og artikkel?
+  
+    hideText.false;
+
+onChange 
+//hvis en knapp trykkes på skjer en endring i hvilken tekst som vises. onChange => funksjon som gjør at den vises
+
+
+
+
+}
+
+export default Actions
